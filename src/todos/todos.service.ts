31420145207f3b998b3ff2dfb850a2a3b7c8fbec
Raj kavankar <rajkavankar@gmail.com>
2025-07-20 20:25:47 +0530

    Fix/container files ownership and logs (#10)
    * fix: reduced image size
    
    * fix: logs setup

--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
 import { CreateTodoDto } from './dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { Todos as Todo } from '@prisma/client';
@@ -8,14 +8,22 @@ import { SuccessResponse } from 'src/common/types';
 
 @Injectable()
 export class TodosService {
+
+    private readonly logger = new Logger(TodosService.name, { timestamp: true });
+
   constructor(private  prismaService: PrismaService){}
+
   async create(createTodoDto: CreateTodoDto): Promise<SuccessResponse<Todo>> {
     try {
       const todo =  await this.prismaService.todos.create({
         data : createTodoDto
       })
+
+      this.logger.log("Todo created successfully")
+
       return {success: true, data: {todo}}
     } catch (error) {
+      this.logger.error(error.stack)
       throw new InternalServerErrorException()
     }
   }
@@ -23,19 +31,30 @@ export class TodosService {
   async findAll(): Promise<SuccessResponse<Todo[]>> {
     try {
       const todos =  await this.prismaService.todos.findMany()
+
+      this.logger.log("Todos fetched successfully")
+
       return {success: true, data: {todos}}
     } catch (error) {
+      this.logger.error(error.stack)
       throw new InternalServerErrorException()
     }
   }
 
   async findOne(id: string) : Promise<SuccessResponse<Todo | null> |null>  {
+
       const todo =  await this.prismaService.todos.findUnique({
         where : {id}
       })
+
       if(todo){
+        this.logger.log(`Todo ${todo.id} fetched successfully`)
+
         return {success: true, data: {todo}}
       }else {
+
+        this.logger.error(`Todo ${id} not found`)
+
        return null
       }
   }
@@ -46,8 +65,12 @@ export class TodosService {
         where : {id},
         data: updateTodoDto
       })
+
+      this.logger.log(`Todo ${todo.id} updated successfully`)
+
       return {success: true, data: {todo}}
     } catch (error) {
+      this.logger.error(error.stack)
       throw new InternalServerErrorException()
     }
   }
@@ -57,8 +80,13 @@ export class TodosService {
       const todo =  await this.prismaService.todos.delete({
         where : {id},      
       })
+
+      this.logger.log(`Todo ${todo.id} deleted successfully`)
+
       return {success: true, data: {message: "todo deleted successfully"}}
+
     } catch (error) {
+      this.logger.error(error.stack)
       throw new InternalServerErrorException()
     }
   }
